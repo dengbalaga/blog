af24bb81668b3071db179e56e3f399a66500cb72
Deng Balaga <dengbalaga@gmail.com>
2020-01-13 18:03:11 +0800

    Updates

---
+++ precache-manifest.c657cb36ea2f366bda995a1d05d987d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a94a77f4b9e2b1ca0e2780f8bc8bb3c0",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "daca911853397b2adc93",
+    "url": "/blog/static/css/main.acb06a32.chunk.css"
+  },
+  {
+    "revision": "b5fab1d4b0937fca6140",
+    "url": "/blog/static/js/2.5e0f29cb.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/blog/static/js/2.5e0f29cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "daca911853397b2adc93",
+    "url": "/blog/static/js/main.bc5709ba.chunk.js"
+  },
+  {
+    "revision": "dce02fc60a0a845b8537",
+    "url": "/blog/static/js/runtime-main.30068621.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blog/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blog/precache-manifest.46e67705799bfc85d95e6a5ab0d3cabb.js"
+  "/blog/precache-manifest.c657cb36ea2f366bda995a1d05d987d1.js"
 );
 
 self.addEventListener('message', (event) => {
