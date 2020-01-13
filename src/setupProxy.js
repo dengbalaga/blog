90cd1ee0cbf5edc3d28f51ea2aa25c17f4bf9db1
Deng Balaga <dengbalaga@gmail.com>
2020-01-13 18:09:27 +0800

    seventh commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,6 +4,7 @@ module.exports = function(app) {
     app.use(
         proxy("/newsarticle", {
             target: "https://www.sti.edu/alumniapi/api",
+            secure: false,
             changeOrigin: true
         })
     );
